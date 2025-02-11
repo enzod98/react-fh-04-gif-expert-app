75c3d492c1e0b7c9a9891e811fb43acc855aabd5
Enzo Duré <enzod98@hotmail.com>
2025-02-10 22:02:38 -0300

    commit inicial

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,14 @@
+
+export const GifExpertApp = () => {
+  return (
+    <>
+        {/* titulo */}
+        <h1>GifExpertApp</h1>
+        
+        {/* Input */}
+
+        {/* Listado de items */}
+            {/* GifItem */}
+    </>
+  )
+}
