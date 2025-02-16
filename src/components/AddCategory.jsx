55d61be1ccabc6a5eaf1a266fa25f9922e46cb8e
Enzo Duré <enzod98@hotmail.com>
2025-02-16 10:31:56 -0300

    Finalizada app de gifs

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import PropTypes from 'prop-types';
+
+
+export const AddCategory = ({ onAddCategory }) => {
+
+    const [inputValue, setInputValue] = useState('')
+    
+    const onInputChange = (event) => {
+        setInputValue(event.target.value);
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if(inputValue.trim().length <= 1) return;
+
+        onAddCategory(inputValue.trim());
+        setInputValue('');
+    }
+
+    return (
+        <form onSubmit={ onSubmit }>
+            <input 
+                type="text" 
+                placeholder="Buscar gifs"
+                value={ inputValue }    
+                onChange={ onInputChange }
+                />
+        </form>
+  )
+}
+
+
+AddCategory.propTypes = {
+    onAddCategory: PropTypes.func.isRequired
+}
